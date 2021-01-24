8ab9b812cb508b68af5dbc12e29fc346d75a3734
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:48:33 -0300

    commit final

---
+++ Aula_1-Variaveis/Calculos/Processamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculos
+{
+    class Processamento
+    {
+        private double num;
+
+        public double Num { get => num; set => num = value; }
+
+        public Processamento( ) { }
+
+        public double numQuadrado( )
+        {
+            return Math.Pow(Num, 2);
+        }
+
+        public double numCubo( )
+        {
+            return Math.Pow(Num, 3);
+        }
+
+        public double numRaiz( )
+        {
+            return Math.Sqrt(Num);
+        }
+
+        public double numPotencia( )
+        {
+            return Math.Pow(Num, 10);
+        }
+    }
+}
---
+++ Aula_1-Variaveis/Calculos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculos
+{
+    class Program
+    {
+        /*
+         9. Faça um algoritmo que receba um
+            número, calcule e mostre:
+            – O número digitado ao quadrado;
+            – O número digitado ao cubo;
+            – A raiz quadrada do número digitado;
+            – O número elevado a potência 10; 
+
+         */
+        static void Main( string[] args )
+        {
+            Processamento p = new Processamento();
+
+            Console.Write("Entre com um Número: ");
+            p.Num = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quadrado: " + p.numQuadrado());
+
+            Console.WriteLine("Cubo: " + p.numCubo());
+
+            Console.WriteLine("Raíz: " + p.numRaiz());
+
+            Console.WriteLine("Potência: " + p.numPotencia());
+        }
+    }
+}
