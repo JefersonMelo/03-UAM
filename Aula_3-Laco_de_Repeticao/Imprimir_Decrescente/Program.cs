42b79f01371b353cd9e4f0a2a44b53f5e7c50f5f
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 15:39:18 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Imprimir_Decrescente/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Imprimir_Decrescente
+{
+    class Program
+    {
+        /*
+        1. Faça um algoritmo que imprima a seguinte
+        sequência: 100, 99, 98, 97, 96, ... 1                           
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 100; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
