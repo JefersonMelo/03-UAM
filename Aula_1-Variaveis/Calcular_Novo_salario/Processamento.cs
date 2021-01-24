bad532d58e5a976ae482b987617e41f305d32f1c
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:13:31 -0300

    commit final

---
+++ Aula_1-Variaveis/Calcular_Novo_salario/Processamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcular_Novo_salario
+{
+    class Processamento
+    {
+        public double calcularSalario( double salario )
+        {
+            salario += salario * 0.25;
+            return salario;
+        }
+    }
+}
---
+++ Aula_1-Variaveis/Calcular_Novo_salario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calcular_Novo_salario
+{
+    class Program
+    {
+        /*Calculo
+        4. Faça um algoritmo que leia o salário de um
+        funcionário. Sabendo que o salário do
+        funcionário teve um aumento de 25%, calcular
+        e mostrar o novo salário. 
+        */
+        static void Main( string[] args )
+        {
+            Processamento p = new Processamento();
+            double salario;
+            Console.Write("Entre com o Salário: ");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Novo Salário: " + p.calcularSalario(salario));
+        }
+    }
+}
