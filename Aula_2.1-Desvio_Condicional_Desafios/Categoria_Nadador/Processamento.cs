5d6a863394da7f772a1f93c36d907d6d8adcd465
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:22:19 -0300

    commit final

---
+++ Aula_2.1-Desvio_Condicional_Desafios/Categoria_Nadador/Processamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Categoria_Nadador
+{
+    class Processamento
+    {
+        //verificar categoria do nadados
+        public string categoriaNadador( int idade )
+        {
+            if (idade > 5 && idade <= 7)
+            {
+                return "Infantil A";
+            }
+            else if (idade > 7 && idade <= 10)
+            {
+                return "Infantil B";
+            }
+            else if (idade > 10 && idade <= 13)
+            {
+                return "Juvenil A";
+            }
+            else if (idade > 13 && idade <= 17)
+            {
+                return "Juvenil B";
+            }
+            else if (idade >= 18)
+            {
+                return "Senior";
+            }
+            return "Não Categorizado";
+        }
+    }
+}
---
+++ Aula_2.1-Desvio_Condicional_Desafios/Categoria_Nadador/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Categoria_Nadador
+{
+    class Program
+    {
+        /*
+         3. Faça um algoritmo que receba a idade de
+            um nadador e imprima a sua categoria
+            seguindo as regras:
+            Categoria Idade
+            Infantil A 5 - 7 anos
+            Infantil B 8 - 10 anos
+            Juvenil A 11- 13 anos
+            Juvenil B 14- 17 anos
+            Sênior maiores de 18 anos
+         */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+            Processamento proces = new Processamento();
+
+            Console.Write("Entre com a Idade: ");
+            variaveis.Num_1_i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(proces.categoriaNadador(variaveis.Num_1_i));
+        }
+    }
+}
