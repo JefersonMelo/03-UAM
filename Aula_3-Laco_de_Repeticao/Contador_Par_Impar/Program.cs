cfec64096311db42b4d82ddfdcf79dbc2dd1522f
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:57:04 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Contador_Par_Impar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Contador_Par_Impar
+{
+    class Program
+    {
+        /*
+        4. Faça um algoritmo que leia 200 números inteiros
+        e imprima quantos são pares e quantos são ímpares.
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 0; i <= 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Par: " + i);
+                }
+                else if (i % 2 != 0)
+                {
+                    Console.WriteLine("Impar: " + i);
+                }
+            }
+        }
+    }
+}
