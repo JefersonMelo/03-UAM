7ef45c1180c2d25cd6dab05796c069b1969e7562
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:10:52 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Se_Maior_Tres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Se_Maior_Tres
+{
+    class Program
+    {
+        /*
+        2. Faça um algoritmo para ler um número inteiro e
+        apresentá-lo, caso não seja maior que três.   
+        */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Número: ");
+            variaveis.NumInt = int.Parse(Console.ReadLine());
+
+            if (variaveis.NumInt > 3)
+            {
+                Console.Write("É maior: " + variaveis.NumInt);
+            }
+            else
+            {
+                Console.Write("Não é Maior que três: " + variaveis.NumInt);
+            }
+        }
+    }
+}
