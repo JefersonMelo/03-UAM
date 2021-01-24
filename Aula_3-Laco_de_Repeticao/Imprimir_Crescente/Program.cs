c4340165846829a2bdc1b0b5035da290ceb5d011
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 15:44:18 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Imprimir_Crescente/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Imprimir_Crescente
+{
+    class Program
+    {
+        /*
+        3. Ler um número inteiro maior que 1 e
+        imprimir na tela a sequência de números
+        que vai do número 1 até o número lido. 
+        */
+
+        static void Main( string[] args )
+        {
+            Console.Write("Num: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 1)
+            {
+                Console.Write("O Num deve ser maior que 1, ao menos.");
+            }
+            else if (num > 1)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
