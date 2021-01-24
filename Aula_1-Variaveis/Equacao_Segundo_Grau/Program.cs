e936a18bd8df78f5b510792bbfd270f50863d9f7
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 16:38:22 -0300

    commit final

---
+++ Aula_1-Variaveis/Equacao_Segundo_Grau/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Equacao_Segundo_Grau
+{
+    class Program
+    {
+        /*
+        3) Faça um algoritmo que leia os coeficientes a, b e c referentes a equação do 
+        segundo grau(ax^2 +bx + c) e exiba como resultado as raízes da equação.
+        */
+
+        static void Main( string[] args )
+        {
+            double a, x, b, c, result;
+            a = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            result = a * Math.Pow(x, 2) + b * x + c;
+            Console.Write("Resultado: " + result);
+        }
+    }
+}
