31f358cf2a1e3f606c3f64714fdfbffed9e44f0e
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:39:08 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Double_Decrescente/Calculos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Double_Decrescente
+{
+    class Calculos
+    {
+        public string compararDoubles( string num, string num_2 )
+        {
+            double numDouble = double.Parse(num);
+            double numDouble_2 = double.Parse(num_2);
+
+            if (numDouble > numDouble_2)
+            {
+                //num = Convert.ToDouble(numDouble).ToString();
+                return num + ", " + num_2;
+            }
+            else if (numDouble < numDouble_2)
+            {
+                return num_2 + ", " + num;
+            }
+
+            return "Iguais";
+        }
+    }
+}
---
+++ Aula_2-Desvio_Condicional/Double_Decrescente/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Double_Decrescente
+{
+    class Program
+    {
+        /*
+         5. Ler dois valores double e apresentá-los em ordem
+            decrescente. Supor que não sejam iguais. 
+        */
+        static void Main( string[] args )
+        {
+            Calculos calculos = new Calculos();
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("1º Num: ");
+            variaveis.NumInt = Console.ReadLine();
+            Console.Write("2º Num: ");
+            variaveis.NumInt_2 = Console.ReadLine();
+
+            Console.Write(calculos.compararDoubles(variaveis.NumInt, variaveis.NumInt_2));
+        }
+    }
+}
