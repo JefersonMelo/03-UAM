ba1530d6e0fcf96705ca4058173c133c470bfb98
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 16:39:48 -0300

    commit final

---
+++ Aula_1-Variaveis/Calculo_Potencia_Motor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculo_Potencia_Motor
+{
+    class Program
+    {
+        /*
+        5) A potência de um motor pode ser expressa em cv ou kW . Sabe-se que
+        1kW = 0,736 cv. Faça um algoritmo que leia um valor de potência em cv e
+        informe o valor de potência equivalente em kW.
+        */
+
+        static void Main( string[] args )
+        {
+            double kW = 0.736;
+            double cv, kwTotal;
+            cv = double.Parse(Console.ReadLine());
+            kwTotal = cv * kW;
+            Console.Write("Resultado: " + kwTotal);
+        }
+    }
+}
