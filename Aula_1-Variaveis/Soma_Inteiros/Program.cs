a5c734f8c9a9058846d062ea8710511eb2bacf4f
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:01:52 -0300

    commit final

---
+++ Aula_1-Variaveis/Soma_Inteiros/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Soma_Inteiros
+{
+    class Program
+    {
+        /*
+        2. Faça um algoritmo que leia quatro números
+            inteiros, calcule e mostre a soma desses
+            números.              
+        */
+
+        static void Main( string[] args )
+        {
+            int num;
+            int soma = 0;
+            Console.Write("Enre com o");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(" " + (i + 1) + "º " + "Número: ");
+                num = int.Parse(Console.ReadLine());
+
+                soma += num;
+
+                if (i == 3)
+                {
+                    Console.WriteLine();
+                    Console.Write("Total: " + soma);
+                }
+            }
+        }
+    }
+}
