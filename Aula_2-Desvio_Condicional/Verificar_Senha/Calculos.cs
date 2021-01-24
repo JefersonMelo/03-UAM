fa77e3db759daea9f3177a0293a298d83aa7a72c
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 13:02:17 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Verificar_Senha/Calculos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Verificar_Senha
+{
+    class Calculos
+    {
+        public string compararSenha( string senha, string senha_2 )
+        {
+            if (senha.Equals(senha_2) == true)
+            {
+                return "Senhas Digitadas Corretamente";
+            }
+
+            return "Senhas Não Conferem. Tete Novamente mais tarde.";
+        }
+    }
+}
---
+++ Aula_2-Desvio_Condicional/Verificar_Senha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Verificar_Senha
+{
+    class Program
+    {
+        /*
+        10.Faça um algoritmo que verifique a
+        validade de uma senha fornecida pelo
+        usuário. Sabendo que a senha é R10p5,
+        imprimir mensagem de “acesso concedido” ou “acesso negado”.           
+        */
+
+        static void Main( string[] args )
+        {
+            Calculos calculos = new Calculos();
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Digite Uma Senha: ");
+            variaveis.NumInt = Console.ReadLine();
+            Console.Write("Digite novamente: ");
+            variaveis.NumInt_2 = Console.ReadLine();
+
+            calculos.compararSenha(variaveis.NumInt, variaveis.NumInt_2);
+        }
+    }
+}
