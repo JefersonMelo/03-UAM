64a8a98a784930f48320698a45fe2541af2ec8a5
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 16:41:00 -0300

    commit final

---
+++ Aula_1-Variaveis/Converter_Temperatura/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Converter_Temperatura
+{
+    class Program
+    {
+        /*
+         6) Faça um algorimo para transformar um valor de temperatura em ºF para ºC,
+            sabendo-se que:C=((F-32)*5)/9    
+         */
+
+        static void Main( string[] args )
+        {
+            double fahrenheit, celcius;
+            fahrenheit = double.Parse(Console.ReadLine());
+            celcius = (fahrenheit - 32) * 5 / 9;
+            Console.Write(celcius.ToString("F2") + "°C");
+        }
+    }
+}
