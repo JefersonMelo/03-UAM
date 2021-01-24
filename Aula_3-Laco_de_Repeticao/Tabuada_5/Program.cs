ae200acc094f9633ea032093a4003514ad468797
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 15:06:33 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Tabuada_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada_5
+{
+    class Program
+    {
+        /*
+        6. Faça um algoritmo que imprima a
+        tabuada do número 5.
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " x " + "5 = " + (i * 5));
+            }
+        }
+    }
+}
