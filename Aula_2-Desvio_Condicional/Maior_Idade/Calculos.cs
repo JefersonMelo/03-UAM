4dd2b01a6f61a9cb20d1038b751010f53a810719
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:49:14 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Maior_Idade/Calculos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maior_Idade
+{
+    class Calculos
+    {
+        public string verificarMaiorIdade( string _idade )
+        {
+            int idade = Convert.ToInt32(_idade);
+
+            if (idade >= 18)
+            {
+                return "Bebe Pinga";
+            }
+            return "Bebe Leite";
+        }
+    }
+}
---
+++ Aula_2-Desvio_Condicional/Maior_Idade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Maior_Idade
+{
+    class Program
+    {
+        /*
+         7. Faça um algoritmo que receba a idade de
+            uma pessoa e mostre se ela é maior de
+            idade ou não.
+        */
+
+        static void Main( string[] args )
+        {
+            Calculos calculos = new Calculos();
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Sua Idade: ");
+            variaveis.NumInt = Console.ReadLine();
+
+            Console.Write(calculos.verificarMaiorIdade(variaveis.NumInt));
+        }
+    }
+}
