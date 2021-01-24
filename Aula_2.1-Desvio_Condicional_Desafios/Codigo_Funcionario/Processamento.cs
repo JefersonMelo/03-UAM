40b7f2f3cbfcec537e15a7659e35f856876b073c
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:27:37 -0300

    commit final

---
+++ Aula_2.1-Desvio_Condicional_Desafios/Codigo_Funcionario/Processamento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codigo_Funcionario
+{
+    class Processamento
+    {
+        //calcular salário
+        public string aumentoSalarial( int codCargo, double salario )
+        {
+            switch (codCargo)
+            {
+                case 1:
+                    salario += salario * (50.0 / 100.0);
+                    return "Escriturário 50% de aumento. Novo salário: " + salario;
+
+                case 2:
+                    salario += salario * (35.0 / 100.0);
+                    return "Secretário 35 % de aumento. Novo salário: " + salario;
+
+                case 3:
+                    salario += salario * (20.0 / 100.0);
+                    return "Caixa 20% de aumento. Novo salário: " + salario;
+
+                case 4:
+                    salario += salario * (10.0 / 100.0);
+                    return "Gerente 10% de aumento. Novo salário: " + salario;
+
+                case 5:
+                    return "5 Diretor Não tem aumento: " + salario;
+
+            }
+
+            return "Código Inválido";
+
+        }
+    }
+}
---
+++ Aula_2.1-Desvio_Condicional_Desafios/Codigo_Funcionario/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Codigo_Funcionario
+{
+    class Program
+    {
+        /*
+         4. Faça um algoritmo que receba o código
+            correspondente ao cargo de um funcionário
+            e imprima seu cargo e o percentual de
+            aumento ao qual este funcionário tem direito
+            seguindo a tabela abaixo:
+            Código Cargo Percentual
+            l Escriturário 50%
+            2 Secretário 35%
+            3 Caixa 20%
+            4 Gerente 10%
+            5 Diretor Não tem aumento 
+         */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+            Processamento proces = new Processamento();
+
+            Console.Write("Digite o Código Para Cargo: " +
+                "\nl Escriturário \n" +
+                "2 Secretário \n" +
+                "3 Caixa \n" +
+                "4 Gerente \n" +
+                "5 Diretor \n" + "Código: ");
+            variaveis.Num_1_i = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o salário: ");
+            variaveis.Num_1_d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(proces.aumentoSalarial(variaveis.Num_1_i, variaveis.Num_1_d));
+        }
+    }
+}
