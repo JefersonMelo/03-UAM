8e98f38b4a941070da0eeda232ebe65b56de473d
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:58:08 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Altura_Sexo_Peso_Ideal/Calculos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altura_Sexo_Peso_Ideal
+{
+    class Calculos
+    {
+        public string calcularPesoIdeal( string _altura, char _sexo )
+        {
+            double altura = Convert.ToDouble(_altura);
+            double pesoIdeal;
+
+            if (_sexo.Equals('m') == true || _sexo.Equals('M') == true)
+            {
+                pesoIdeal = 72.7 * altura - 58;
+
+                return "Masculino: " + pesoIdeal;
+            }
+
+            pesoIdeal = 62.1 * altura - 44.7;
+
+            return "Feminino: " + pesoIdeal;
+
+        }
+    }
+}
---
+++ Aula_2-Desvio_Condicional/Altura_Sexo_Peso_Ideal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Altura_Sexo_Peso_Ideal
+{
+    class Program
+    {
+        /*
+        9. Faça um algoritmo que leia a altura e o
+           sexo de uma pessoa, calcule e mostre seu
+           peso ideal. Sabendo que:
+           – Para homens: (72,7 * altura) – 58
+           – Para mulheres (62,1 * altura) – 44,7           
+        */
+
+        static void Main( string[] args )
+        {
+            Calculos calculos = new Calculos();
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Sua Altura: ");
+            variaveis.NumInt = Console.ReadLine();
+            Console.Write("Entre com Sexo: ");
+            variaveis.Sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(calculos.calcularPesoIdeal(variaveis.NumInt, variaveis.Sexo));
+        }
+    }
+}
