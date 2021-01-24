8269fb032e90610de53f8f364c5a82b59372fa79
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:05:30 -0300

    commit final

---
+++ Aula_1-Variaveis/Area_Triangulo/Processamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area_Triangulo
+{
+    class Processamento
+    {
+        public double calcularTriangulo( double baase, double altura )
+        {
+            double area;
+            area = (baase * altura) / 2;
+            return area;
+        }
+    }
+}
---
+++ Aula_1-Variaveis/Area_Triangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Area_Triangulo
+{
+    class Program
+    {
+        /*Calculo
+        * 5. Construir um algoritmo que calcule a área
+        de um triângulo retângulo dado a base e a
+        altura.
+        ( at= (altura*base) / 2 ). 
+        */
+
+        static void Main( string[] args )
+        {
+            Processamento p = new Processamento();
+            double baase, altura;
+            Console.Write("Entre com a Base: ");
+            baase = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com a Altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Triangulo: " + p.calcularTriangulo(baase, altura));
+        }
+    }
+}
