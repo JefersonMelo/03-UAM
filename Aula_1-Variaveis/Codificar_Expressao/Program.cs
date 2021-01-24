644ad04d1d2a2c506b4f243d4e8d76bf36d25d0b
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 16:36:19 -0300

    commit final

--- Aula_1-Variaveis/Codificar_Expressao/Program.cs
+++ Aula_1-Variaveis/Codificar_Expressao/Program.cs
@@ -8,6 +8,8 @@ namespace Codificar_Expressao
         {
 
             double respA, respB, respC;
+            int A = 10, B = 3, X = 2, Y = 1;
+
             Console.Write("Num: ");
             int x = int.Parse(Console.ReadLine());
             int y = int.Parse(Console.ReadLine());
@@ -21,7 +23,21 @@ namespace Codificar_Expressao
             Console.WriteLine("A)" + respA +
             "\nB)" + respB +
             "\nC)" + respC);
-
+            
+            //2. Sabendo-se que A=10, B=3, X=2 e Y=1, quais os resultados fornecidos pelas expressões abaixo:
+            //a) X+Y-A + (A –B^2+Y) – 4*X
+            respA = X + Y - A + (A - Math.Pow(B, 2) + Y) - 4 * X;
+            Console.WriteLine("a)" + respA);
+            //b) B ^ (2 + X) – A / 3 + 1
+            respB = Math.Pow(B, 2 + X) - A / 3 + 1;
+            Console.WriteLine("b)" + respB);
+            //c) (A –B^3 >X) OR (X – 3 =1)
+            respC = A - Math.Pow(B, 3);
+            if (respC > X || (X - 1) == 1){Console.WriteLine("c)" + 0);}
+            else{Console.WriteLine("cc)" + respC);}
+            //d) NOT (X*Y = B)
+            if (!(X * Y == B)){Console.WriteLine("d)Não");}
+            else{Console.WriteLine("d)Sim");}
         }
     }
 }
