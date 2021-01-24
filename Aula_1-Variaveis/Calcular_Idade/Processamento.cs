e1b465516cf95129a1d0774db268b793e0f35f31
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:26:25 -0300

    commit final

---
+++ Aula_1-Variaveis/Calcular_Idade/Processamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcular_Idade
+{
+    class Processamento
+    {        
+        public Processamento( ) { }
+
+        private string anoAtual = DateTime.Now.Year.ToString();
+        public string AnoAtual { get => anoAtual; set => anoAtual = value; }
+
+        public int calcularIdade( int ano )
+        {
+            int idade;
+            string anoAtual = AnoAtual;
+            idade = int.Parse(anoAtual) - ano;
+            return idade;
+        }
+    }
+}
---
+++ Aula_1-Variaveis/Calcular_Idade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calcular_Idade
+{
+    class Program
+    {
+        /*Calculo
+         7. Construir um algoritmo que leia o ano de
+            nascimento de uma pessoa e o ano atual,
+            calcule e mostre:
+            – A idade dessa pessoa;
+            – Quantos anos essa pessoa terá em 2025   
+         */
+
+        static void Main( string[] args )
+        {
+            Processamento p = new Processamento();
+
+            Console.Write("Ano de Nascimento: ");
+            int nascimento = int.Parse(Console.ReadLine());
+
+            Console.Write("Ano Atual: " + p.AnoAtual +
+                "\nNascimento: " + nascimento +
+                "\nIdade: " + p.calcularIdade(nascimento) + " Anos" +
+                "\nIdade em 2025: " + (p.calcularIdade(nascimento) + 5) + " Anos" +
+                "\n");
+        }
+    }
+}
