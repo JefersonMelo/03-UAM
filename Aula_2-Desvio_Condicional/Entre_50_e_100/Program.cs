c9fced707c1e53cfcd6ed664394a272746e8a8c2
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:24:55 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Entre_50_e_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Entre_50_e_100
+{
+    class Program
+    {
+        /*
+        3. Faça um algoritmo para ler um número inteiro e
+            imprimir o seu valor caso esteja no intervalo entre
+            50 e 100 (inclusive).            
+        */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Dado: ");
+            variaveis.NumInt = int.Parse(Console.ReadLine());
+
+            if (variaveis.NumInt > 50 && variaveis.NumInt <= 100)
+            {
+                Console.Write("Num: " + variaveis.NumInt);
+            }
+            else
+            {
+                Console.Write("Não é Maior");
+            }
+        }
+    }
+}
