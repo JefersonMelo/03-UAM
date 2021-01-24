a50e006473f407e547a854fb97fb38d60964fd54
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 16:11:45 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Calcular_Idade_Altura_Conjunto_Pessoas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Calcular_Idade_Altura_Conjunto_Pessoas
+{
+    class Program
+    {
+        /*
+        7. Faça um algoritmo que receba as idades,
+        pesos e alturas de um conjunto de
+        pessoas, calcule e mostre:
+        § A média das idades de todas as pessoas
+        § A quantidade de pessoas com peso superior
+        a 90kg e altura inferior a 1,50 metro
+        § A porcentagem de pessoas com idade entre
+        10 e 30 anos entre as pessoas que medem
+        mais de 1,90 m 
+        */
+
+        static void Main( string[] args )
+        {
+            Variaveis v = new Variaveis();
+
+            do
+            {
+                //contador
+                v.Cont_1++;
+
+                //idades, pesos e alturas
+                Console.Write("\nIdade: ");
+                v.Num_1_int = int.Parse(Console.ReadLine());
+                //Verificar Encerramento
+                if (v.Num_1_int < 1) { break; }
+                Console.Write("Peso: ");
+                v.Num_1_double = double.Parse(Console.ReadLine());
+                Console.Write("Altura: ");
+                v.Num_2_double = double.Parse(Console.ReadLine());
+
+                //§ A média das idades de todas as pessoas
+                v.Soma += v.Num_1_int;
+                v.Media = v.Soma / v.Cont_1;
+
+                //§ A quantidade de pessoas com peso superior a 90kg e altura inferior a 1,50 metro
+                if (v.Num_1_double >= 90.0 && v.Num_2_double <= 1.50)
+                {
+                    v.Cont_2++;
+                }
+                //§ Número de pessoas com idade entre 10 e 30 anos entre as pessoas que medem mais de 1,90 m
+                else if (v.Num_1_int >= 10 || v.Num_1_int <= 30 && v.Num_2_double >= 1.90)
+                {
+                    //contador
+                    v.Num_2_int++;
+
+                }
+
+            } while (v.Num_1_int < 1);
+
+            //§ A porcentagem de pessoas com idade entre 10 e 30 anos entre as pessoas que medem mais de 1,90 m
+            v.Porcentagem = ((v.Num_2_int / v.Cont_1) - 1) * 100;
+
+            Console.Write("Qtd Pessoas Peso > 90kg e Altura < 1,50: " + v.Cont_2);
+            Console.Write("A porcentagem de pessoas com idade entre 10 e 30 anos " +
+                    "entre as pessoas que medem mais de 1,90: " + v.Porcentagem);
+        }
+    }
+}
---
+++ Aula_3-Laco_de_Repeticao/Calcular_Idade_Altura_Conjunto_Pessoas/Variveis.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcular_Idade_Altura_Conjunto_Pessoas
+{
+    class Variaveis
+    {
+        //inteiros
+        private int num_1_int;
+        private int num_2_int;
+        private int cont_1;
+        private int cont_2;
+        private int totalPessoas;
+        //ponto flutuante
+        private double num_1_double;
+        private double num_2_double;
+        private double media;
+        private double soma;
+        private double soma_2;
+        private double porcentagem;
+        private double pesoTotal;
+        //texto
+        private string dado_1_string;
+        private string dado_2_string;
+        //caracter
+        private char dado_1_char;
+        private char dado_2_char;
+        //data
+        private DateTime data;
+
+        public int Num_1_int { get => num_1_int; set => num_1_int = value; }
+        public int Num_2_int { get => num_2_int; set => num_2_int = value; }
+        public int Cont_1 { get => cont_1; set => cont_1 = value; }
+        public int Cont_2 { get => cont_2; set => cont_2 = value; }
+        public int TotalPessoas { get => totalPessoas; set => totalPessoas = value; }
+
+        public double Num_1_double { get => num_1_double; set => num_1_double = value; }
+        public double Num_2_double { get => num_2_double; set => num_2_double = value; }
+        public double Media { get => media; set => media = value; }
+        public double Soma { get => soma; set => soma = value; }
+        public double Porcentagem { get => porcentagem; set => porcentagem = value; }
+        public double PesoTotal { get => pesoTotal; set => pesoTotal = value; }
+        public double Soma_2 { get => soma_2; set => soma_2 = value; }
+
+        public string Dado_1_string { get => dado_1_string; set => dado_1_string = value; }
+        public string Dado_2_string { get => dado_2_string; set => dado_2_string = value; }
+
+        public char Dado_1_char { get => dado_1_char; set => dado_1_char = value; }
+        public char Dado_2_char { get => dado_2_char; set => dado_2_char = value; }
+
+        public DateTime Data { get => data; set => data = value; }
+
+    }
+}
