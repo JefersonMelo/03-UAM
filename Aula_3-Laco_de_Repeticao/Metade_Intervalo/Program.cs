9e008530ff3deed5644e38aa31bf43b4aea7ee10
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:54:34 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Metade_Intervalo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Metade_Intervalo
+{
+    class Program
+    {
+        /*
+        3. Faça um algoritmo que imprima a metade de
+        cada número no intervalo de 10 a 20.
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 10; i <= 20; i++)
+            {
+                double div;
+                div = (double)i / 2;
+                Console.WriteLine(div.ToString("F2"));
+            }
+        }
+    }
+}
