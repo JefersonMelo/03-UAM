ae711eb50a6624253fb6ed02c3f21cae00d9bf45
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:18:50 -0300

    commit final

---
+++ Aula_2.1-Desvio_Condicional_Desafios/Operacoes_Matematicas/Processamento.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operacoes_Matematicas
+{
+    class Processamento
+    {
+        //calculadora básica
+        public string calculadora( char sinal, string num_1, string num_2 )
+        {
+            int num1 = Convert.ToInt32(num_1);
+            int num2 = Convert.ToInt32(num_2);
+
+            if (sinal == '+')
+            {
+                int soma = num1 + num2;
+                return "Soma: " + soma;
+            }
+            if (sinal == '-')
+            {
+                int subtracao = num1 - num2;
+                return "Subtração: " + subtracao;
+            }
+            if (sinal == '*')
+            {
+                if (num1 == 0 || num2 == 0)
+                {
+                    return "Por Zero, sempre Zero";
+                }
+                else
+                {
+                    double multiplicacao = (double)num1 * num2;
+                    return "Multiplicação: " + multiplicacao;
+                }
+
+            }
+            if (sinal == '/')
+            {
+                if (num1 == 0 || num2 == 0)
+                {
+                    return "Por Zero, sempre Zero";
+                }
+                else
+                {
+                    double divisao = (double)num1 / num2;
+                    return "Divisão: " + divisao;
+                }
+
+            }
+            if (sinal == '%')
+            {
+                if (num1 == 0 || num2 == 0)
+                {
+                    return "Por Zero, sempre Zero";
+                }
+                else
+                {
+                    double resto = (double)num1 % num2;
+                    return "Subtração: " + resto;
+                }
+
+            }
+
+            return "Sinal Inválido: | " + sinal + " | Use um dos sinais: | + | - | * | / | % |";
+        }
+    }
+}
---
+++ Aula_2.1-Desvio_Condicional_Desafios/Operacoes_Matematicas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operacoes_Matematicas
+{
+    class Program
+    {
+        /*
+        2. Faça um algoritmo para realizar as
+            operações matemáticas de soma,
+            subtração, divisão, multiplicação e resto.
+            § O algoritmo deve ler dois números e o sinal
+            correspondente à operação desejada (usar char),
+            no final deve ser impresso o resultado.
+            § Restrições:
+            Ø Se o sinal digitado não corresponder a uma operação
+            apresentar mensagem “Sinal Inválido” e finalizar
+            Ø Para as operações de divisão e resto da divisão
+            verificar se o divisor é válido (maior que zero)! 
+        */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+            Processamento proces = new Processamento();
+
+            Console.Write("Entre coma operação: ");
+            variaveis.Caracter = char.Parse(Console.ReadLine());
+            Console.Write("1º Valor: ");
+            variaveis.Num_1_s = Console.ReadLine();
+            Console.Write("2º Valor: ");
+            variaveis.Num_2_s = Console.ReadLine();
+
+            Console.WriteLine(proces.calculadora(variaveis.Caracter, variaveis.Num_1_s, variaveis.Num_2_s));
+        }
+    }
+}
