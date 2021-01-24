b970dbd08d83f9c37e73a0ede18a1504a77c66f5
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:31:19 -0300

    commit final

---
+++ Aula_2.1-Desvio_Condicional_Desafios/Carga_Pesada/Processamento.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carga_Pesada
+{
+    class Processamento
+    {
+        // Calcular Frete Por peso da carga
+        public double valorImpostoPorEstado( string codEstado, int peso, string codCarga )
+        {
+            double imposto;
+            double freteTotal;
+            if (codEstado.ToUpper() == "PR")
+            {
+                imposto = 35.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) * imposto;
+                return freteTotal;
+            }
+            else if (codEstado.ToLower() == "RS")
+            {
+                imposto = 25.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) * imposto;
+                return freteTotal;
+            }
+            else if (codEstado.ToLower() == "RJ")
+            {
+                imposto = 15.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) * imposto;
+                return freteTotal;
+            }
+            else if (codEstado.ToLower() == "ES")
+            {
+                imposto = 5.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) * imposto;
+                return freteTotal;
+            }
+            else if (codEstado.ToLower() == "SP")
+            {
+                imposto = 1.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) * imposto;
+                return freteTotal;
+            }
+
+            return 1;
+        }
+
+        public double valorTotalFreteComImpostoEstado( int peso, string codEstado, string codCarga )
+        {
+            double imposto;
+            double freteTotal;
+            if (codEstado.ToUpper() == "PR")
+            {
+                imposto = 35.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) +
+                    (valorFreteSemImpostoEstado(peso, codCarga) * imposto);
+                return freteTotal;
+            }
+            else if (codEstado.ToUpper() == "RS")
+            {
+                imposto = 25.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) +
+                    (valorFreteSemImpostoEstado(peso, codCarga) * imposto);
+                return freteTotal;
+            }
+            else if (codEstado.ToUpper() == "RJ")
+            {
+                imposto = 15.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) +
+                    (valorFreteSemImpostoEstado(peso, codCarga) * imposto);
+                return freteTotal;
+            }
+            else if (codEstado.ToUpper() == "ES")
+            {
+                imposto = 5.0 / 100.0;
+                freteTotal = valorFreteSemImpostoEstado(peso, codCarga) +
+                    (valorFreteSemImpostoEstado(peso, codCarga) * imposto);
+                return freteTotal;
+            }
+            else if (codEstado.ToUpper() == "SP")
+            {
+                return valorFreteSemImpostoEstado(peso, codCarga);
+            }
+
+            return 0;
+        }
+
+        public double valorFreteSemImpostoEstado( int peso, string codCarga )
+        {
+            double total = 0.0;
+            // categoria C
+            if (peso >= 10 || peso <= 20)
+            {
+                total = converterToneladaQuilo(peso) * categoriaValor(codCarga);
+                return total;
+            }
+            // categoria B
+            else if (peso >= 21 || peso <= 30)
+            {
+                total = converterToneladaQuilo(peso) * categoriaValor(codCarga);
+                return total;
+            }
+            // categoria A
+            else if (peso >= 31 || peso <= 40)
+            {
+                total = converterToneladaQuilo(peso) * categoriaValor(codCarga);
+                return total;
+            }
+
+            return total;
+        }
+
+        public double converterToneladaQuilo( int tonelada )
+        {
+            double quilo;
+            quilo = tonelada * 1000;
+
+            return quilo;
+        }
+
+        public int categoriaValor( string codCarga )
+        {
+            if (codCarga.ToUpper() == "A")
+            {
+                return 34;
+            }
+            if (codCarga.ToUpper() == "B")
+            {
+                return 20;
+            }
+            if (codCarga.ToUpper() == "C")
+            {
+                return 10;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ Aula_2.1-Desvio_Condicional_Desafios/Carga_Pesada/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Carga_Pesada
+{
+    class Program
+    {
+        /*
+        5. Carga Pesada
+            • Construir um algoritmo que receba:
+            § O código do estado de origem da carga de um caminhão;
+            § O peso da carga do caminhão em toneladas e;
+            § O código da carga.
+            § Restrição: Validar as entradas código do estado e código da carga!!
+            Código	Estado	 IMPOSTO	
+            PR	    Paraná       35%	
+            RS	    Rio G. Sul   25%	
+            RJ	    Rio De Jan   15%	
+            ES	    Espírito S   5%	
+            SP	    São Paulo    ISENTO	
+            CÓDIGO DA CARGA	    PESO T     PREÇO POR QUILO	
+                    C         10 a 20	    R$ 100	
+                    B         21 a 30       R$ 250	
+                    A         31 a 40       R$ 340	
+            Desafio 01: Carga Pesada
+            • Calcular e mostrar:
+            § O peso da carga do caminhão convertido em quilos
+            § O preço da carga do caminhão
+            § O valor do imposto, sabendo-se que o
+            imposto é cobrado sobre o preço da carga do
+            caminhão e depende do estado de origem
+            § O valor total transportado pelo caminhão
+            (preço da carga + impostos)
+        */
+
+        static void Main( string[] args )
+        {
+            Variaveis variaveis = new Variaveis();
+            Processamento proces = new Processamento();
+
+            Console.Write("Estado: ");
+            variaveis.Num_1_s = Console.ReadLine();
+            Console.Write("Entre com o Peso em Toneladas: ");
+            variaveis.Num_1_i = int.Parse(Console.ReadLine());
+            Console.Write("Entre com a Categoria, sendo A, B ou C: ");
+            variaveis.Num_2_s = Console.ReadLine();
+
+            Console.Write("Valor do Imposto Por Estado: " + proces.valorImpostoPorEstado(variaveis.Num_1_s, variaveis.Num_1_i, variaveis.Num_2_s) + "\n");
+            Console.Write("Peso Convertido de Tonelada Para Quilos: " + proces.converterToneladaQuilo(variaveis.Num_1_i) + "\n");
+            Console.Write("Valor do Frete Sem Imposto do Estado: " + proces.valorFreteSemImpostoEstado(variaveis.Num_1_i, variaveis.Num_2_s).ToString("C") + "\n");
+            Console.Write("Valor do Frete Com Imposto do Estado: " + proces.valorTotalFreteComImpostoEstado(variaveis.Num_1_i, variaveis.Num_1_s, variaveis.Num_2_s).ToString("C") + "\n");
+        }
+    }
+}
