0b98f00c622da350c13cd1a25198aa171f219c87
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:40:00 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Imprimir_Contador/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Imprimir_Contador
+{
+    class Program
+    {
+        /*
+        1. Faça um algoritmo que imprima a seguinte
+        sequência de números de 1 a 100.
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
