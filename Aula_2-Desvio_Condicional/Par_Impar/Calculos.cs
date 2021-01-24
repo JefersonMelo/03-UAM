86580d0b5c1d8e03d3056223ef329195ce0ba587
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 12:53:26 -0300

    commit final

---
+++ Aula_2-Desvio_Condicional/Par_Impar/Calculos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Par_Impar
+{
+    class Calculos
+    {
+        public string verificarSePar( string _num )
+        {
+            int num = Convert.ToInt32(_num);
+
+            if ((num % 2) == 0)
+            {
+                return "Par";
+            }
+            return "Impar";
+        }
+    }
+}
---
+++ Aula_2-Desvio_Condicional/Par_Impar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Par_Impar
+{
+    class Program
+    {
+        /*
+         8. Ler um número inteiro e apresentar uma
+            mensagem informando se o número é par
+            ou ímpar. (usar o operador módulo (%) )                    
+         */
+
+        static void Main( string[] args )
+        {
+            Calculos calculos = new Calculos();
+            Variaveis variaveis = new Variaveis();
+
+            Console.Write("Num: ");
+            variaveis.NumInt = Console.ReadLine();
+
+            Console.Write(calculos.verificarSePar(variaveis.NumInt));
+        }
+    }
+}
