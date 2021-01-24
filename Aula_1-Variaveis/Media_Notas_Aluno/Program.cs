6674773d3f7d62d569c5d9723c7e0321746b61ea
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:10:45 -0300

    commit final

---
+++ Aula_1-Variaveis/Media_Notas_Aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Media_Notas_Aluno
+{
+    class Program
+    {
+        /*
+         3. Faça um algoritmo que leia três notas de um
+             aluno, calcule e mostre a média aritmética do
+             aluno.             
+         */
+
+        static void Main( string[] args )
+        {
+            double nota, soma = 0;
+            Console.Write("Entre com a ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write((i + 1) + "º " + "Nota: ");
+                nota = double.Parse(Console.ReadLine());
+                soma += nota;
+                if (i == 2)
+                {
+                    nota = soma / 3;
+                    Console.WriteLine();
+                    Console.Write("Média: " + nota);
+                }
+            }
+        }
+    }
+}
