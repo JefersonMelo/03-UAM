8f7e34c5a35aaf71ffbd24f798043b302c610fe1
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 14:43:20 -0300

    commit final

---
+++ Aula_3-Laco_de_Repeticao/Imprimir_Entre_100_200/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Imprimir_Entre_100_200
+{
+    class Program
+    {
+        /*
+        2. Escreva um algoritmo que imprima todos os
+        números inteiros de 100 a 200.
+        */
+
+        static void Main( string[] args )
+        {
+            for (int i = 100; i <= 200; i++)
+            {
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
