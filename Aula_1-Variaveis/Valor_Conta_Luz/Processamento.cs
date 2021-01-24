98c4085193fa04b705fbe617b3ba155250f3dc12
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-24 11:37:16 -0300

    primeiro commit

---
+++ Aula_1-Variaveis/Valor_Conta_Luz/Processamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valor_Conta_Luz
+{
+    class Processamento
+    {
+        public Processamento( ) { }
+
+        private int qtdWatt;
+        private double valorUnidadeWatt;
+        private double salarioMinimo;
+        private double num;
+
+        public int QtdWatt { get => qtdWatt; set => qtdWatt = value; }
+        public double ValorUnidadeWatt { get => valorUnidadeWatt; set => valorUnidadeWatt = value; }
+        public double SalarioMinimo { get => salarioMinimo; set => salarioMinimo = value; }
+        public double Num { get => num; set => num = value; }
+
+        public double calcularQuilowatt( double salarioMinimo )
+        {
+            salarioMinimo = SalarioMinimo;
+            valorUnidadeWatt = (SalarioMinimo / 500);
+            return valorUnidadeWatt;
+        }
+
+        public double calcularContaEletrica( int qtdWatt )
+        {
+            qtdWatt = QtdWatt;
+            double valorConta = valorUnidadeWatt * qtdWatt;
+            return valorConta;
+        }
+    }
+}
---
+++ Aula_1-Variaveis/Valor_Conta_Luz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Valor_Conta_Luz
+{
+    class Program
+    {
+        /*Calculo
+            8. Sabe-se que um quilowatt de energia
+            custa 1/500 avos do salário mínimo.
+            Faça um algoritmo que receba o valor
+            do salário mínimo e a quantidade de
+            quilowatts consumida por uma
+            residência. Calcule e mostre:
+            A. O valor, em reais, de cada quilowatt;
+            B. O valor, em reais, a ser pago por essa residência 
+        */
+
+        static void Main( string[] args )
+        {
+            Processamento p = new Processamento();
+
+            Console.Write("Salário Mínimo Vingente: ");
+            p.SalarioMinimo = double.Parse(Console.ReadLine());
+            Console.Write("Entre com a Quantidade de QuiloWatts Gasta: ");
+            p.QtdWatt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor Unidade Quilowatt: " + p.calcularQuilowatt(p.SalarioMinimo));
+            Console.WriteLine("Valor da Conta de Luz: " + p.calcularContaEletrica(p.QtdWatt));
+
+            double valorDesconto = p.calcularContaEletrica(p.QtdWatt) - p.calcularContaEletrica(p.QtdWatt) * 0.15;
+            Console.WriteLine("15% de Desconto: " + valorDesconto);
+        }
+    }
+}
